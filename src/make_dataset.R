a7309f76f1ea47d4c2ede4459a55bef3bb18febc
gab <gabriele.bonomiboseggia@unicreditgoup.eu>
2018-03-07 17:26:09 +0100

    source folder

---
+++ src/make_dataset.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+n <- 1e3
+dim <- 2
+df <- mlbench::mlbench.circle(n, dim)
+df <- mlbench.spirals(300,1.5,0.05)
+
+res <- data_frame(
+  x = df$x[,1],
+  y = df$x[,2],
+  class = df$classes
+)
+
+res %>% ggplot(aes(x = x, y = y, fill = class), color = "black") + 
+  geom_point(shape = 21) + theme_minimal() + labs(x = "", y = "", fill = "") +
+  theme(legend.position = "none")
