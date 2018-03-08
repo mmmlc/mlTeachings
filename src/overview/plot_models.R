8e88a1b6931e939364a21211aca92adf5cc76835
gbonomib <gabriele1.bonomi@mail.polimi.it>
2018-03-08 23:36:23 +0100

    Idea for a plotting function. To be improved for sure: 1) correct use of shape 21 that would properly accept "color" and "fill" attributes 2) choice filling palette 3) background takes some time to be processed.

--- src/overview/plot_models.R
+++ src/overview/plot_models.R
@@ -1,17 +1,29 @@
-source('src/lib.R')
+plot_models = function(model, res, from = -1.5, to = 1.5, by = 0.05, breaks = 7, ...){
+  
+  grid = expand.grid(x = seq(from, to, by),
+                     y = seq(from, to, by)
+  ) 
+  
+  gridProb = model$modelInfo$prob(modelFit = model$finalModel,
+                                  newdata = grid)[,1]
+  
+  gridProb = gridProb %>% cut(breaks)
+  
+  grid = data_frame(x = grid$x, 
+                    y = grid$y,
+                    z = gridProb)
+  
 
-plot_models = function(model, ...){}
+  ggplot() + ggtitle(model$method) +
+    geom_raster(data = grid, aes(x = x, y = y, fill = z)) +
+    scale_fill_brewer(palette="Spectral") +
+    geom_point(data = res, aes(x = x, y = y, color = class),  shape = 16) + # to be fixed using shape 21!!!
+    # scale_color_manual(values = c('black','black')) +
+    scale_fill_brewer(palette="Spectral", direction = -1) +
+    theme_minimal() + labs(x = "", y = "", fill = "") +
+    theme(legend.position = "none") 
+  
+}
 
-#### rege...
+# plot_models(svm, res)
 
-grid = expand.grid(seq(from = -10, to = 10, by = 0.1), seq(from = -10, to = 10, by = 0.1))
-toBePlot = svm %>% predict(grid, type = 'prob')
-
-grid %>% ggplot(aes(x = Var1, y = Var2)) + geom_raster(aes(fill = toBePlot$A))+
-  geom_contour(aes(z = toBePlot$A, colour = 'black')) +
-  theme_minimal() + labs(x = "", y = "", fill = "") +
-  theme(legend.position = "none")
-
-res %>% ggplot(aes(x = x, y = y, fill = class), color = "black") + 
-  geom_point(shape = 21) + theme_minimal() + labs(x = "", y = "", fill = "") +
-  theme(legend.position = "none")
