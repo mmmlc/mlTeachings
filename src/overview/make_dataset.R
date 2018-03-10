8caf9279672c5edc8ec4f27c8ef8ab822735d5ca
Fabio Colombo <colombo.fabio@gmail.com>
2018-03-10 19:06:51 +0100

    Added a function to build inner circles and a function to standardize datasets and prepare them to be plotted with ggplot

--- src/overview/make_dataset.R
+++ src/overview/make_dataset.R
@@ -2,15 +2,59 @@ source('src/lib.R')
 
 n <- 1e3
 dim <- 2
-df <- mlbench::mlbench.circle(n, dim)
-df <- mlbench.spirals(300,1.5,0.05)
 
-res <- data_frame(
-  x = df$x[,1],
-  y = df$x[,2],
-  class = df$classes
+make_circles <- function(n, ratio = 0.3, sd = 0.1) {
+  rad <- seq(0, 2 * pi, length.out = ceiling(n / 2))
+  outer_circle <- matrix(c(cos(rad), sin(rad)),ncol = 2)
+  inner_circle <- ratio * outer_circle[1:(n - nrow(outer_circle)), ]
+  
+  res <- rbind(
+    outer_circle,
+    inner_circle
+  )
+  assertthat::are_equal(nrow(res), n)
+  
+  if (sd != 0) {
+    res <- res + matrix( rnorm(2 * nrow(res),sd = sd), ncol = 2)
+  }
+  
+  return(
+    list(
+      x = res,
+      classes = c(rep(1, nrow(outer_circle)), rep(2, nrow(inner_circle)))
+    )
+  )
+}
+
+prepare_dataset <- function(df, type = NULL) {
+  range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+  res <- data_frame(
+    x = df$x[, 1] %>% range01,
+    y = df$x[, 2] %>% range01,
+    class = str_c("class_", df$classes)
+  )
+  
+  if(!is.null(type)) {
+    res$type = type
+  }
+  
+  return(res)
+}
+
+datasets <- list(
+  "normal" = mlbench.2dnormals(n, sd = 0.3),
+  "make_circles" = make_circles(n, 0.3, 0.1)
 )
 
-res %>% ggplot(aes(x = x, y = y, fill = class), color = "black") + 
-  geom_point(shape = 21) + theme_minimal() + labs(x = "", y = "", fill = "") +
-  theme(legend.position = "none")
+full_df <- lapply(
+  names(datasets),
+  function(x) {prepare_dataset(datasets[[x]], x)}
+) %>% bind_rows
+
+
+full_df %>% ggplot(aes(x = x, y = y, color = class)) +
+  geom_point() + facet_wrap(~type) + scale_color_fivethirtyeight() + theme_fivethirtyeight() +
+  labs(color = "") + theme(legend.position = "none")
+
+
+
