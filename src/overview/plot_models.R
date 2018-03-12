270c857b57d8227bb9fa165797c9d5c63044e9fd
gbonomib <gabriele1.bonomi@mail.polimi.it>
2018-03-12 21:57:31 +0100

    plot_models.R fixes

--- src/overview/plot_models.R
+++ src/overview/plot_models.R
@@ -1,4 +1,4 @@
-plot_models = function(model, res, from = 0, to = 1, by = 0.005, breaks = 7, ...){
+plot_models = function(model, res, from = 0, to = 1, by = 0.005, breaks = 5, ...){
   
   grid = expand.grid(x = seq(from, to, by),
                      y = seq(from, to, by)
@@ -14,20 +14,23 @@ plot_models = function(model, res, from = 0, to = 1, by = 0.005, breaks = 7, ...
                     z = gridProb_b,
                     contour = gridProb)
   
-
   ggplot() + ggtitle(model$method) +
-    geom_contour(data = grid, aes(x = x, y = y, z = contour)) +
-    geom_raster(data = grid, aes(x = x, y = y, fill = z)) +
-    scale_fill_brewer(palette="Spectral") +
+    geom_raster(data = grid, aes(x = x, y = y, fill = z), interpolate = T) +
+    scale_fill_brewer(palette="RdYlBu") +
+    # geom_contour(data = grid, aes(x = x, y = y, z = contour),
+    #              bins = (breaks -1),
+    #              colour = 'gray',
+    #              size = 1) +
     geom_point(data = res, aes(x = x, y = y),
                color = 'black',
                fill = ifelse(res$class == 'class_1', 'blue', 'red'),
                shape = 21) +
-    # scale_color_manual(values = c('black','black')) +
-    theme_minimal() + labs(x = "", y = "", fill = "") +
-    theme(legend.position = "none") 
+    theme_fivethirtyeight() + labs(x = "", y = "", fill = "") +
+    theme(legend.position = "none")
+  
   
 }
 
-plot_models(svm, res)
 
+
+plot_models(svm, res)
