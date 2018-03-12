b44ca059dcef55c042c3123c5b6ede3467cc6897
gbonomib <gabriele1.bonomi@mail.polimi.it>
2018-03-13 00:41:03 +0100

    finalized main.R

--- src/main.R
+++ src/main.R
@@ -4,20 +4,18 @@ source('src/overview/train_models.R')
 source('src/overview/plot_models.R')
 
 
-methods = c('svmRadial', 'svmLinear', 'qda', 'knn', 'rf', 'nb' )
+methods = c('svmRadial', 'svmLinear', 'qda', 'knn', 'rf', 'nb', 'ada' )
 # 'logreg', 'adaboost'
 
 data = get_partitioned_df()
 
 # train selected models ####
-model = train_models(methods = methods)
+model = train_models(methods = 'ada')
 
 
 # plot model of choice ####
-plot_models(model$nb$spirals$model, data$spirals$full_val)
+plot_models(model$nb$spirals$model, data$spirals$full)
 
 # confusion matrix and other stats ####
 model$rf$spirals$confusion_matrix
 
-
-data$circles$full_val$type %>% unique()
