51b063396ce5c2ac34303b338f209b2906671b94
Fabio Colombo <colombo.fabio@gmail.com>
2018-03-13 22:56:15 +0100

    update the plot models - first version of combined plots. To improve graphics

--- src/main.R
+++ src/main.R
@@ -14,7 +14,10 @@ model = train_models(methods = methods)
 
 #TODO plot side by side?
 # plot model of choice ####
-plot_models(model$rf$circles$model, data$circles$full_val)
+
+### remove full
+data[["full"]] <- NULL
+plot_models(data, model)
 
 # confusion matrix and other stats ####
 model$rf$spirals$confusion_matrix
--- src/overview/plot_models.R
+++ src/overview/plot_models.R
@@ -1,39 +1,68 @@
-plot_models = function(model,
-                       df,
-                       from = 0, to = 1,
-                       by = 0.005,
-                       breaks = 7,
-                       title = NULL, ...){
+plot_models <- function(data, model){
+  ## Prepare plot dataset
+  data_df <- lapply(
+    names(data),
+    function(data_name) {
+      df <- data[[data_name]] 
+      rbind(
+        df$full_train %>% mutate(partition = "train"),
+        df$full_val %>% mutate(partition = "val")
+      )
+    }
+  ) %>% bind_rows
   
-  grid = expand.grid(x = seq(from, to, by),
-                     y = seq(from, to, by)
-  ) 
+  ## expand data_df for each model
+  model_df <- data_frame(
+    model = names(model),
+    tmp = 1
+  )
   
-  gridProb = model$modelInfo$prob(modelFit = model$finalModel,
-                                  newdata = grid)[,1]
+  data_df <- data_df %>% mutate(tmp = 1) %>% left_join(model_df) %>% select(-tmp)
   
-  gridProb_b = gridProb %>% cut(breaks)
+  ### prepare prediction
+  from <- 0
+  to <- 1
+  n <- 100
+  grid_df <- expand.grid(
+    x = seq(from, to, length.out = n),
+    y = seq(from, to, length.out = n),
+    type = data_df$type %>% unique,
+    model = data_df$model %>% unique
+  )
   
-  grid = data_frame(x = grid$x, 
-                    y = grid$y,
-                    z = gridProb_b,
-                    contour = gridProb)
+  grid_df$prob <- 0
+  for(model_name in names(model)) {
+    print(model_name)
+    for(data_name in names(data)) {
+      print(data_name)
+      new_data_df <- grid_df[grid_df$model == model_name & grid_df$type == data_name,] %>% select(x,y)
+      predict_type <- "probabilities"
+      if(model_name %in% c("knn", "rf")) {
+        predict_type <- "prob"
+      }
+      prob <- predict(model[[model_name]][[data_name]]$model$finalModel, newdata = small_df, type = predict_type)
+      if(is.matrix(prob)) {
+        prob <- prob[,1]
+      } else {
+        prob <- prob$posterior[,1]
+      }
+      grid_df[grid_df$model == model_name & grid_df$type == data_name,]$prob <- prob
+    }
+  }
   
-  if(is.null(title)){title = paste(model$method, df$type %>% unique(), sep = ' | ')}
-  
-  ggplot() + ggtitle(title) +
-    geom_raster(data = grid, aes(x = x, y = y, fill = z), interpolate = T) +
-    scale_fill_brewer(palette="RdYlBu") +
-    # geom_contour(data = grid, aes(x = x, y = y, z = contour),
-    #              bins = (breaks -1),
-    #              colour = 'gray',
-    #              size = 1) +
-    geom_point(data = df, aes(x = x, y = y),
-               color = 'black',
-               fill = ifelse(df$class == 'class_1', 'blue', 'red'),
-               shape = 21) +
-    theme_fivethirtyeight() + labs(x = "", y = "", fill = "") +
-    theme(legend.position = "none")
-  
-  
+  ## plot data
+  ggplot() + 
+    geom_raster(data = grid_df, aes(x = x, y = y, fill = prob), interpolate = T, alpha = 0.5) +
+    scale_fill_gradient2(low = "blue", mid = "white",
+                         high = "red", midpoint = 0.5, space = "rgb",
+                         na.value = "grey50", guide = "colourbar") +
+    geom_point(data = data_df, aes(x = x, y = y, color = class, alpha = partition)) + 
+    scale_alpha_manual(values = c("train" = 0.6, "test" = 1)) +
+    facet_grid(type ~ model) + 
+    theme(legend.position = "none", panel.grid.minor = element_blank(),
+          panel.background = element_blank(), axis.line = element_blank(),
+          axis.title=element_blank(),
+          axis.text=element_blank(),
+          axis.ticks=element_blank()) + 
+    labs(x = "", y = "")
 }
