9dadf00b1f4d9137d56586b01196f9f61a61a29e
gbonomib <g.bonomib@gmail.com>
2018-03-14 12:37:35 +0100

    added random forest and logistic regression
    modified plot_models.R for allowing the plot of the logistic_regression.R and a small tweak for flexibility

--- src/overview/main_overview.R
+++ src/overview/main_overview.R
@@ -1,12 +1,15 @@
 source('src/lib.R')
 
+methods = list()
 
-methods = list('linearSvm' = get_linearSvm_model,
+methods = list('logReg' = get_logisticRegression_model,
+               'linearSvm' = get_linearSvm_model,
                'radialSvm' = get_radialSvm_model,
                'qda' = get_qda_model,
                'knn' = get_knn_model,
                'rf' = get_rf_model
-)
+               )
+
 
 # 'logreg', 'adaboost'
 
@@ -14,11 +17,14 @@ methods = list('linearSvm' = get_linearSvm_model,
 # train selected models ####
 model = train_models(methods = methods)
 
-
 # plot trained models ####
 
 data = get_partitioned_df(include_full = F)
 plot_models(data, model)
 
+model$glm$normal$y_prob
+
 # confusion matrix and other stats ####
 model$rf$spirals$confusion_matrix
+
+
