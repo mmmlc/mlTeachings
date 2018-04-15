f7fb381f198899dd41c312578bbb533b19125a55
gbonomib <gabriele1.bonomi@mail.polimi.it>
2018-04-15 14:55:18 +0200

    added mlp and nnet model.

---
+++ src/overview/nnet.R
@@ -0,0 +1,24 @@
+get_nnet_model <- function(x_train,
+                                y_train,
+                                x_val = NULL,
+                                y_val = NULL){
+  
+  
+  model = list()
+  
+  model$model = train(x = x_train,
+                      y = y_train,
+                      method = 'nnet',
+                      maxit = 500,
+                      trControl = trainControl(classProbs =  TRUE))
+
+  if(!is.null(x_val) & !is.null(y_val)){
+
+    model$y_class = model$model %>% predict(newdata = x_val, type = 'raw')
+    model$y_prob = model$model %>% predict(newdata = x_val, type = 'prob')
+    # model$confusionMatrix = confusionMatrix(model$y_class, y_val)
+    model$y_class = model$y_class %>% as_data_frame()
+  }
+  
+  return(model)
+}
