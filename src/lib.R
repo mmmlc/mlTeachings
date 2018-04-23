da11ff08ae79c2b0916215ec76deeee13a499cfd
Fabio Colombo <colombo.fabio@gmail.com>
2018-04-23 11:24:59 +0200

    Revert "strip off data & code from teaser branch"
    This reverts commit a25d2826c942dd0280acd1bdf134325c3ad022a7.

--- src/lib.R
+++ src/lib.R
@@ -7,10 +7,22 @@ require(assertthat)
 require(kernlab)
 require(rattle)
 
+# install_keras()
+
 source('src/overview/make_dataset.R')
 
 source('src/overview/train_models.R')
 source('src/overview/plot_models.R')
 
+source('src/overview/linear_svm.R')
 source('src/overview/radial_svm.R')
+source('src/overview/polynomial_svm.R')
+source('src/overview/qda.R')
+source('src/overview/knn.R')
 source('src/overview/rf.R')
+source('src/overview/logistic_regression.R')
+source('src/overview/decision_tree.R')
+source('src/overview/gaussian_process.R')
+source('src/overview/mlp.R')
+source('src/overview/nnet.R')
+source('src/overview/fancyRPartPlot.R')
